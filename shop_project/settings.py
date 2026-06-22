2ca036026725062f80f756beb968731cc3a6f7be
maxim <maxzyuzkov@gmail.com>
2026-06-22 21:19:50 +0300

    .

--- shop_project/settings.py
+++ shop_project/settings.py
@@ -14,9 +14,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # ==========================================
 SECRET_KEY = config('SECRET_KEY', default='django-insecure-default-key-for-local-dev')
 DEBUG = config('DEBUG', default=True, cast=bool)
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1').split(',')
-ALLOWED_HOSTS = [host.strip() for host in ALLOWED_HOSTS] 
-
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+    'shopproject-production-6699.up.railway.app',
+]
 # ==========================================
 # ПРИЛОЖЕНИЯ
 # ==========================================
